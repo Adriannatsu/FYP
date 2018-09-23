8728acd3cd6116525eed8b8d884dee6490a381b8
Adriannatsu <cheeseburgerfly@gmail.com>
2018-09-23 14:33:08 +0800

    firstversion

---
+++ AndroidConnectPHP/app/src/main/java/app/AppConfig.java
@@ -0,0 +1,8 @@
+package app;
+
+public class AppConfig {
+
+    public static String URL_LOGIN = "http://192.168.1.5/android_login_api/login.php";
+
+    public static String URL_REGISTER = "http://192.168.1.5/android_login_api/register.php";
+}
